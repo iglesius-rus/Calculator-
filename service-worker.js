ca8f81db09e80d49bf8ad5756e74323b01d7f267
iglesius-rus <vovanovicivan@gmail.com>
2025-09-18 09:10:36 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+// Simple offline cache
+const CACHE = 'nordcheb-1.017';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
